ad5b440b3c55268fbfb49f2c4a255f43851143d8
N7thz <nathanferreiradev@gmail.com>
2025-07-06 10:18:30 -0300

    Improves layout responsiveness with padding adjustments
    Enhances the main content area by adding responsive padding for smaller screens, improving user experience on mobile devices.
    
    No functional changes were made to the components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,15 @@ export default function Home() {
   return (
     <div className="flex-1 flex flex-col justify-between">
       <Header />
-      <main className="flex-1 flex items-center justify-center">
+      <main className="flex-1 flex items-center justify-center max-sm:px-4">
         <Card className="w-full sm:max-w-[425px] bg-background">
           <CardHeader>
             <RadioGroup />
           </CardHeader>
           <Form />
-          <Separator className="data-[orientation=horizontal]:w-[90%] mx-auto" />
+          <Separator
+            className="data-[orientation=horizontal]:w-[90%] mx-auto"
+          />
           <Footer />
         </Card>
       </main>
